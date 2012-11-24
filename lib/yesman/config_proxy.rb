6ae67f519c444580b9bcc45746275c85aa49e01c
Jason Madsen <knomedia@gmail.com>
2012-11-24 14:01:09 -0700

    Adds simple config saving and loading to .yesman/config
    This may need to be more complex eventually. For now it saves the params (either default or passed in)

---
+++ lib/yesman/config_proxy.rb
@@ -0,0 +1,38 @@
+require 'yaml'
+
+class ConfigProxy 
+  
+  attr_reader :params
+  attr_reader :config_path
+
+  def initialize
+    @params = nil
+    @config_path = ".yesman/config"
+  end
+
+  def create_config params
+    verify_config_exists
+    File.open( config_path, "w" ) do |file| 
+      file.write params.to_yaml
+    end
+    @params = params;
+  end
+
+  def load_config
+    verify_config_exists
+    @params = YAML::load_file config_path
+  end
+
+  def params 
+    if @params == nil
+      load_config
+    end
+    @params
+  end
+
+  private
+  def verify_config_exists
+    FileUtil.ensure_file config_path
+  end
+  
+end
