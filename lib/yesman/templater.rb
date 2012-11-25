6b9342bd2540138fdaf15fccbcd7669417f02585
Jason Madsen <knomedia@gmail.com>
2012-11-24 20:37:48 -0700

    Adds basic method for templating
    The templater is repsonsible for blending erb templates and data and returning the string output

---
+++ lib/yesman/templater.rb
@@ -0,0 +1,22 @@
+require 'erb'
+
+class Templater
+  
+  def merge class_type, template_path
+    @class_type = class_type
+    @template_path = template_path
+
+    load_template
+    apply_class_to_template
+    @output
+  end
+
+  def load_template
+    @loaded_template = ERB.new File.read( @template_path )
+  end
+
+  def apply_class_to_template
+   @output = @loaded_template.run( @class_type.get_binding ) 
+  end
+  
+end
